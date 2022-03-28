3f8b79c4f037de4b904f50da1ec77af0c5356c3b
Alessio1921 <caruso.alessio1999@gmail.com>
2022-03-28 17:28:18 +0200

    dynamic nav bar addition

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,18 +4,11 @@
       <div id="logo-img">
         <img src="../../img/dc-logo.png" alt="">
       </div>
-      <ul>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-        <li><a href="">item</a></li>
-      </ul>
+      <nav>
+        <ul>
+          <li v-for="(item,index) in listNav" :key="index" :class="{active:item.active}"><a :class="{active:item.active}" :href="item.url">{{item.name}}</a></li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -23,8 +16,58 @@
 <script>
 export default {
   name: 'homeHeader',
-  
+  data: function(){
+  return {
+    listNav:[
+      {
+        name:"characters",
+        active:false,
+        url:"#"
+      },
+      {
+        name:"comics",
+        active:true,
+        url:"#"
+      },
+      {
+        name:"movies",
+        active:false,
+        url:"#"
+      },
+      {
+        name:"games",
+        active:false,
+        url:"#"
+      },
+      {
+        name:"collectibles",
+        active:false,
+        url:"#"
+      },
+      {
+        name:"videos",
+        active:false,
+        url:"#"
+      },
+      {
+        name:"fans",
+        active:false,
+        url:"#"
+      },
+      {
+        name:"news",
+        active:false,
+        url:"#"
+      },
+      {
+        name:"shop",
+        active:false,
+        url:"#"
+      }
+    ]
+  }
 }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -45,6 +88,11 @@ export default {
       li{
         list-style: none;
         margin: 0 .7rem;
+        .active{
+          border-bottom: 4px solid rgb(56,128,241);
+          color: rgb(56,128,241);
+          padding-bottom:33px ;
+        }
         a{
           text-decoration: none;
           color: black;
